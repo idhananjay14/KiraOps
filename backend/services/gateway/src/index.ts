48919751cadbb5f0f19b1a5d2ff07007d06f4286
Dhananjay <dhananjaybhardwaj1416@gmail.com>
2026-07-07 04:46:31 +0000

    feat(gateway): implement API gateway

---
+++ backend/services/gateway/src/index.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "Gateway is healthy",
+    });
+});
+
+app.use(
+    "/api/products",
+    createProxyMiddleware({
+        target: process.env.PRODUCT_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: (path) => `/products${path}`,
+    })
+);
+
+app.use((req, res) => {
+    res.status(404).json({
+        message: "Route not found",
+    });
+});
+
+app.use((
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction) => {
+    console.error(err);
+
+    res.status(500).json({
+        message: "Internal server error",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Gateway running on port ${PORT}`);
+});
